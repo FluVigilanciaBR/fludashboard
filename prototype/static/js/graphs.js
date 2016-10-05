c26f7b896664c1a8051a67e5a2eb32456a95e32e
Ivan Ogasawara <ivan.ogasawara@gmail.com>
2016-10-05 19:25:42 -0300

    fixed week link (sliderXchart); using queue to get data; fixed link week slider

--- prototype/static/js/graphs.js
+++ prototype/static/js/graphs.js
@@ -1,69 +1,312 @@
-queue()
-    .defer(d3.json, "static/data/br-states.json")
-    .defer(d3.json, "/data/incidence-color-alerts/" + $('#interval').val())
-    .defer(d3.json, "/data/weekly-incidence-curve/" + $('#interval').val())
-    .await(makeGraphs);
+var flu_colors = {
+    1: 'green',
+    2: 'yellow',
+    3: 'orange',
+    4: 'red',
+};
 
-function getColor(d) {
-    return d == 4 ? '#ff0000' :
-           d == 3 ? '#ff9900' :
-           d == 2 ? '#f0ff00' :
-                    '#30c130';
+/**
+ * 
+ * 
+ */
+function load_graphs() {
+    var year = $('#year').val() || 0;
+    var week = $('#week').val() || 0;
+    var state = $('#selected_state').val() || '';
+    
+    queue()
+        .defer(d3.json, 'static/data/br-states.json')
+        .defer(d3.json, '/data/incidence/' + year)
+        .await(makeGraphs);
 }
 
-function makeGraphs(
-    error, statesJson, incidenceColorAlerts, incidence
-) {
-    //Create a Crossfilter instance
-    var ndx = crossfilter(incidence);
-
-    //Define Dimensions
-    var stateDim = ndx.dimension(function(d) { return d["UF"]; });
-    var incidenceDim = ndx.dimension(function(d) { return d["incidence"]; });
-    var alertDim = ndx.dimension(function(d) {return d['alert']});
-
-    //Calculate metrics
-    var totalIncidenceByState = stateDim.group().reduceSum(function(d) {
-        return d["incidence"];
+/**
+ * 
+ * 
+ */
+function plot_incidence_chart(year, state_name, week) {
+    if (state_name == '') {
+        $('#chart-stage').addClass('hidden');
+        return;
+    } else {
+        $('#chart-stage').removeClass('hidden');
+    }
+    
+    var chart = c3.generate({
+        bindto: '#weekly-incidence-curve-chart',
+        data: {
+          url: './data/weekly-incidence-curve/' + year + '/' + state_name,
+          x: 'isoweek',
+          names: {
+              corredor_baixo: null,
+              corredor_mediano: null,
+              corredor_alto: null,
+              srag: 'SRAG',
+              limiar_pre_epidemico: 'Limiar Pré epidêmico',
+              intensidade_alta: 'Intensidade Alta',
+              intensidade_muito_alta: 'Intensidade Muito Alta'
+          },
+          types: {
+              corredor_baixo: 'area',
+              corredor_mediano: 'area',
+              corredor_alto: 'area',
+              srag: 'line',
+              limiar_pre_epidemico: 'line',
+              intensidade_alta: 'line',
+              intensidade_muito_alta: 'line'
+          },
+          colors: {
+            corredor_baixo: '#00ff00',
+            corredor_mediano: '#ffff00',
+            corredor_alto: '#ff9900',
+            srag: '#000000',
+            limiar_pre_epidemico: '#0000ff',
+            intensidade_alta: '#00ff00',
+            intensidade_muito_alta: '#ff0000'
+          }
+        },
+        axis: {
+          x: {
+            label: {
+              text: 'SE',
+              position: 'outer-center'
+            }
+          },
+          y: {
+            label: {
+              text: 'Incidência (por 100 mil habitantes)',
+              position: 'outer-middle'
+            }
+          },
+        },
+        regions: [
+            {start:0, end:52, class: 'alert-red'}
+        ],
+        grid: {                 
+          x: {
+           lines: [
+              {value: week, text: 'Semana Selecionada', position: 'middle'}
+            ], show: false
+          },
+          y: {show: false}
+        },/*
+        zoom: {
+          enabled: true
+        },*/ /*
+        subchart: {
+          show: true
+        },*/
+        tooltip: {
+          show: false
+        },
+        point: {
+          show: false
+        }
     });
 
-    var all = ndx.groupAll();
-    var totalIncidence = ndx.groupAll().reduceSum(function(d) {return d["incidence"];});
+}
+
+/**
+ * Red if the incidence was above the high threshold for at least 5 weeks;
+ * Orange if above the high threshold from 1 to 4 weeks;
+ * Yellow if crossed the epidemic threshold but not the high one;
+ * Green if it did not cross the epidemic threshold.
+ */
+function alert_level_for_whole_year(d) {
+    if (d[4] >= 5) return 4;
+    if (d[4] >= 1 && d[4] < 5) return 3;
+    if (d[2] >= 1 || d[3] >= 1) return 2;
+    return 1;
+}
+
+/**
+ * 
+ * 
+ */
+function makeMap(br_states, srag_data) {
+    var map = L.map('map');
+    week = parseInt($('#week').val() || 0);
+    
+    map.setView([-16, -50.528742], 3);
+    
+    // create the tile layer with correct attribution
+    var osmUrl='http://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png';
+    var osmAttrib='Map data © <a href="http://openstreetmap.org">OpenStreetMap</a> contributors';
+    var osm = new L.TileLayer(osmUrl, {minZoom: 1, maxZoom: 12, attribution: osmAttrib});
+    
+    function onEachFeature(feature, layer) {
+        //bind click
+        layer.on({
+            click: function(){
+                // reset line weight
+                geojson_layer.eachLayer(function (_layer) {  
+                    _layer.setStyle({
+                        weight: 1
+                    });
+                });
+                // bold the selected state
+                layer.setStyle({
+                    weight: 2
+                });
+                
+                var state_name = feature.properties.nome;
+                var week = parseInt($('#week').val() || 0);
+                var year = parseInt($('#year').val() || 0);
+                
+                $('#selected_state').val(state_name);
+                $('#incidence-chart-title').text(' - ' + state_name);
+
+                plot_incidence_chart(year, state_name, week);
+            }
+        });
+    };
+    
+    // start the map in South-East England
+    geojson_layer = L.geoJson(br_states, {
+        onEachFeature: onEachFeature,
+        style: function(feature) {
+            l_name = feature.properties.nome;
+            
+            style_properties= {
+                fillColor: '#ffffff',
+                color: '#333333',
+                fillOpacity: 0.5,
+                weight: 1,
+            };
+            
+            week_state = $.grep(srag_data, function(n,i){
+                return (
+                    n.unidade_da_federacao===l_name &&
+                    n.isoweek===week
+                );
+            })[0];
+            
+            style_properties['fillColor'] = flu_colors[week_state['alert']];
+            return style_properties;
+        }
+    });
+    geojson_layer.addTo(map);
+    osm.addTo(map);
+}
 
 
-    //Charts
-    var brChart = dc.geoChoroplethChart("#br-chart");
-    var numberIncidenceND = dc.numberDisplay("#number-incidence-nd");
-    var totalIncidenceND = dc.numberDisplay("#total-incidence-nd");
+/**
+ * 
+ * 
+ */
+function changeColorMap(df) {
+    var state = $('#selected_state').val();
+    var week = parseInt($('#week').val() || 0);
+    var style_properties= {
+        fillColor: '#ffffff',
+        color: '#333333',
+        fillOpacity: 0.5,
+        weight: 1,
+    };
+    
+    
+    if(week>0) {
+        df_alert = $.grep(df, function(n,i){
+            return n.isoweek===week
+        });
+        
+        geojson_layer.eachLayer(function (layer) {  
+            l_name = layer.feature.properties.nome;
+            
+            layer.setStyle(style_properties);
+            if (l_name==state) {
+                layer.setStyle({'weight': 2});
+            }
+        
+            df_alert_state = $.grep(df_alert, function(n,i){
+                return n.unidade_da_federacao===l_name
+            })[0];
+            
+            layer.setStyle({
+                fillColor: flu_colors[df_alert_state['alert']]
+            });
+        });
+    } else {
+        geojson_layer.eachLayer(function (layer) {  
+            l_name = layer.feature.properties.nome;
+            
+            layer.setStyle(style_properties);
+            if (l_name==state) {
+                layer.setStyle({'weight': 2});
+            }
+            
+            alerts = {
+                1: 0,
+                2: 0,
+                3: 0,
+                4: 0
+            };
+            
+            df_alert_state = $.grep(df, function(n,i){
+                return n.unidade_da_federacao===l_name
+            });
+            
+            
+            $(df_alert_state).each(function(i){ 
+                ++alerts[df_alert_state[i]['alert']];
+            });
+            
+            layer.setStyle({
+                fillColor: flu_colors[alert_level_for_whole_year(alerts)]
+            });
+        
+        });
+    }
+}
 
-    numberIncidenceND
-        .formatNumber(d3.format("d"))
-        .valueAccessor(function(d){return d; })
-        .group(all);
+/**
+ * 
+ * 
+ */
+function changeWeek(srag_data) {
+    d3.select('#week').on('change', function(){ 
+        week = parseInt($('#week').val() || 0);
+        year = parseInt($('#year').val() || 0);
 
-    totalIncidenceND
-        .formatNumber(d3.format("d"))
-        .valueAccessor(function(d){return d; })
-        .group(totalIncidence)
-        .formatNumber(d3.format(".3s"));
+        $('#table-week').text(week);
+        $('#week-filter-display').text(week);
+        
+        style_properties = {
+            fillColor: 'white',
+            weight: 1
+        };
+        
+        if(week>0) {
+            $("#data-table").removeClass('hidden');
+            df = $.grep(srag_data, function(n,i){
+                return n.isoweek==week
+            });
+        } else {
+            $("#data-table").addClass('hidden');
+            $("#linechart").addClass('hidden');
+            $('#table-week').text('');
+            df = srag_data;
+        }
+        
+        //print_filter(df);
+        changeColorMap(srag_data);
+        
+        state_name = $('#selected_state').val();
 
-    brChart.width(1000)
-        .height(330)
-        .dimension(stateDim)
-        .group(totalIncidenceByState)
-        .colors(['#ff0000', '#ff9900', '#f0ff00', '#30c130'])
-        .colorDomain([1, 4])
-        .overlayGeoJson(statesJson["features"], "state", function (d) {
-            return d.properties.nome;
-        })
-        .projection(d3.geo.equirectangular()
-                    .scale(600)
-                    .translate([-10, -30]))
-        .title(function (p) {
-            return "State: " + p["key"]
-                    + "\n"
-                    + "Total Incidence: " + Math.round(p["value"]) + " $";
-        })
+        plot_incidence_chart(year, state_name, week);
 
-    dc.renderAll();
-};
+    });
+}
+
+/**
+ * 
+ * 
+ */
+function makeGraphs(
+    error, br_states, srag_data
+) {    
+    makeMap(br_states, srag_data);
+    changeWeek(srag_data);
+}
+
+$(document).ready(load_graphs);
