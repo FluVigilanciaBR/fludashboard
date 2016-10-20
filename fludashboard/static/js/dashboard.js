8d37dd2eb7f93d05e964e9e0de9dc74fcf4c2d44
Ivan Ogasawara <ivan.ogasawara@gmail.com>
2016-10-19 22:39:56 -0200

    refactoring frontend files

--- fludashboard/static/js/dashboard.js
+++ fludashboard/static/js/dashboard.js
@@ -1,193 +1,172 @@
+/* -*- Mode: JavaScript; tab-width: 8; indent-tabs-mode: nil; c-basic-offset: 2 -*- */
+/* vim: set ts=8 sts=2 et sw=2 tw=80: */
+/* This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at http://mozilla.org/MPL/2.0/. */
+
 /** Dashboard class */
 class Dashboard {
-    constructor(
-        userInterface, map
-    ) {
-        this.ui = userInterface;
+  constructor(
+      userInterface, mapId
+  ) {
+    this.group_by = 'week'; // year or week
+    this.separated_by = 'state'; // state or region
 
-        this.group_by = 'week'; // year or week
-        this.separated_by = 'state'; // state or region
-        this.flu_colors = {
-            1: 'green',
-            2: 'yellow',
-            3: 'orange',
-            4: 'red',
-        };
+    // table
+    this.sragTable = new SRAGTable();
+    this.sragMap = new SRAGMap();
+    this.sragChart = new SRAGChart('#weekly-incidence-curve-chart');
 
-        // create the tile layer with correct attribution
-        this.map = L.map('map');
-        this.osmUrl='http://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png';
-        this.osmAttrib='Map data © <a href="http://openstreetmap.org">' +
-            'OpenStreetMap</a> contributors';
-        this.osm = new L.TileLayer(
-            this.osmUrl, {minZoom: 1, maxZoom: 12, attribution: this.osmAttrib}
-        );
+    $('#year').change(this.load_graphs);
+  }
 
-        // cheat
-        this.divTableContent = '' +
-        '<table id="data-table" ' +
-        '  class="display table table-striped table-condensed">' +
-        '  <!-- create a custom header -->' +
-        '  <thead class="header">' +
-        '    <tr class="header">' +
-        '      <th>Unidade da Federa&ccedil;&atilde;o</th>' +
-        '      <th>Semana</th>' +
-        '      <th>Incidência (por 100 mil habitantes)</th>' +
-        '    </tr>' +
-        '  </thead>' +
-        '</table>';
+  /**
+   * start up the charts
+   *
+   */
+  init() {
+    this.load_graphs();
+    $('.week-display').text($('#week').val() || 0);
+  }
 
-        this.divTable.html(this.divTableContent);
-        this.data_table = this.divTable.DataTable();
-        this.yearInput.change(this.load_graphs);
+  geoJsonURL() {
+    if ($('#radTypeState').attr('checked')) {
+      return 'static/data/br-states.json';
+    } else {
+      return 'static/data/br-regions.json';
+    }
+  }
+
+  /**
+   * load data and build charts
+   *
+   */
+  load_graphs() {
+    if ($('#radTypeState').attr('checked')) {
+      var url = 'static/data/br-states.json';
+    } else {
+      var url = 'static/data/br-regions.json';
     }
 
-    /**
-     * start up the charts
-     *
-     */
-    init() {
-        this.map.setView([-16, -50.528742], 3);
-        this.osm.addTo(this.map);
-        this.load_graphs();
-        this.weekDisplay.text(this.ui.week);
+    var _this = this;
+
+    var fn = function(error, statesBR, sragData){
+      _this.makeGraphs(error, statesBR, sragData);
+    };
+
+    queue()
+      .defer(d3.json, url)
+      .defer(d3.json, '/data/incidence/' + ($('#year').val() || 0))
+      .await(fn);
+  }
+
+
+  /**
+   * Plot incidence chart using
+   * @param {number} year - year to filter the data
+   * @param {string} state_name - state_name to filter the data
+   * @param {number} week- week to filter the data
+   */
+  plot_incidence_chart(year, state_name, week) {
+    if (state_name == '') {
+      $('.chart-incidence').addClass('hidden');
+      $('.weekly-incidence-curve-chart').empty();
+      return;
+    } else {
+      $('.chart-incidence').removeClass('hidden');
     }
 
-    get geoJsonURL() {
-        if (this.typeGeoStateInput.attr('checked')) {
-            return 'static/data/br-states.json';
-        } else {
-            return 'static/data/br-regions.json';
-        }
-    }
+    SRAGChart.plot(
+      '#weekly-incidence-curve-chart',
+      year, state_name, week
+    );
+  }
 
-    /**
-     * load data and build charts
-     *
-     */
-    load_graphs() {
-        queue()
-            .defer(d3.json, this.geoJsonUrl)
-            .defer(d3.json, '/data/incidence/' + year)
-            .await(makeGraphs);
-    }
+  /**
+   * Get the alert level color using the follow criteria:
+   *
+   * Red (level 4) if the incidence was above the high threshold for at
+   *  least 5 weeks;
+   * Orange (level 3) if above the high threshold from 1 to 4 weeks;
+   * Yellow (level 2) if crossed the epidemic threshold but not the high one;
+   * Green (level 1) if it did not cross the epidemic threshold.
+   * @param {dict} d - total number of alert occurrence
+   * @return {number} alert level
+   */
+  getAlertLevelForWholeYear(d) {
+    if (d[4] >= 5) return 4;
+    if (d[4] >= 1 && d[4] < 5) return 3;
+    if (d[2] >= 1 || d[3] >= 1) return 2;
+    return 1;
+  }
 
+  /**
+   * When a week mark is changed, this function should update and trigger
+   * some information and chart
+   * @param {dict} sragData - srag data
+   */
+  changeWeek(sragData) {
+    d3.select('#week').on('change', function(){
+      var week = parseInt($('#week').val() || 0);
+      var year = parseInt($('#year').val() || 0);
 
-    /**
-     * Plot incidence chart using
-     * @param {number} year - year to filter the data
-     * @param {string} state_name - state_name to filter the data
-     * @param {number} week- week to filter the data
-     */
-    plot_incidence_chart(year, state_name, week) {
-        if (state_name == '') {
-            $('.chart-incidence').addClass('hidden');
-            $('#vi .weekly-incidence-curve-chart').empty();
-            return;
-        } else {
-            $('.chart-incidence').removeClass('hidden');
-        }
+      $('.week-display').text(week);
 
-        SRAGChart.plot(
-            '#weekly-incidence-curve-chart',
-            year, state_name, week
-        );
-    }
+      style_properties = {
+        fillColor: 'white',
+        weight: 1
+      };
 
-    /**
-     * Get the alert level color using the follow criteria:
-     *
-     * Red (level 4) if the incidence was above the high threshold for at
-     *  least 5 weeks;
-     * Orange (level 3) if above the high threshold from 1 to 4 weeks;
-     * Yellow (level 2) if crossed the epidemic threshold but not the high one;
-     * Green (level 1) if it did not cross the epidemic threshold.
-     * @param {dict} d - total number of alert occurrence
-     * @return {number} alert level
-     */
-    getAlertLevelForWholeYear(d) {
-        if (d[4] >= 5) return 4;
-        if (d[4] >= 1 && d[4] < 5) return 3;
-        if (d[2] >= 1 || d[3] >= 1) return 2;
-        return 1;
-    }
-}
+      if(week>0) {
+        df = $.grep(sragData, function(n,i){
+            return n.isoweek==week
+        });
+      } else {
+        $("#linechart").addClass('hidden');
+        $('.week-display').text('');
+        df = sragData;
+      }
 
+      //print_filter(df);
+      state_name = $('#selected-state').val();
 
-/**
- * When a week mark is changed, this function should update and trigger
- * some information and chart
- * @param {dict} srag_data - srag data
- */
-function changeWeek(srag_data) {
-    d3.select('#week').on('change', function(){ 
-        week = parseInt($('#week').val() || 0);
-        year = parseInt($('#year').val() || 0);
-
-        $('.week-display').text(week);
-        
-        style_properties = {
-            fillColor: 'white',
-            weight: 1
-        };
-        
-        if(week>0) {
-            $("#data-table").removeClass('hidden');
-            df = $.grep(srag_data, function(n,i){
-                return n.isoweek==week
-            });
-        } else {
-            $("#data-table").addClass('hidden');
-            $("#linechart").addClass('hidden');
-            $('.week-display').text('');
-            df = srag_data;
-        }
-        
-        //print_filter(df);
-        changeColorMap(srag_data);
-        
-        state_name = $('#selected_state').val();
-
-        plot_incidence_chart(year, state_name, week);
-        makeTable(year, week, state_name);
-
+      this.sragMap.changeColorMap(sragData);
+      this.sragChart.plot(year, state_name, week);
+      this.sragTable.makeTable(year, week, state_name);
     });
-}
+  }
 
-
-
-/**
- * 
- * 
- */
-function makeGraphs(
-    error, br_states, srag_data
-) {    
-    makeMap(br_states, srag_data);
-    changeWeek(srag_data);
-    
-    var state_name = $('#selected_state').val();
+  /**
+   *
+   *
+   */
+  makeGraphs(
+    error, statesBR, sragData
+  ) {
+    var state_name = $('#selected-state').val();
     var week = parseInt($('#week').val() || 0);
     var year = parseInt($('#year').val() || 0);
+    var _this = this;
+
+    this.sragMap.makeMap(statesBR, sragData, year, week,
+      function(
+        year, stateName, week
+      ) {
+        _this.sragChart.plot(year, stateName, week);
+        _this.sragTable.makeTable(year, week, stateName);
+      }
+    );
+    this.changeWeek(sragData);
 
     if (state_name) {
-        plot_incidence_chart(year, state_name, week);
+      this.sragChart.plot(year, state_name, week);
     }
-    
-    makeTable(year, week, state_name);
+
+    this.sragTable.makeTable(year, week, state_name);
+  }
 }
 
-userInterface = UserInterface({
-    year: $('#year'),
-    week: $('#week'),
-    state: $('#selected_state'),
-    tableContainer: $('#divTable'),
-    weekDisplay: $('.week-display'),
-    radTypeState: $('#radTypeState')
-});
 
-dashboard = new Dashboard(
-    userInterface
-);
+dashboard = new Dashboard();
 
-$(document).ready(dashboard.init);
+$(document).ready(function(){dashboard.init()});
--- fludashboard/static/js/srag_map.js
+++ fludashboard/static/js/srag_map.js
@@ -1,159 +1,183 @@
+/* -*- Mode: JavaScript; tab-width: 8; indent-tabs-mode: nil; c-basic-offset: 2 -*- */
+/* vim: set ts=8 sts=2 et sw=2 tw=80: */
+/* This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at http://mozilla.org/MPL/2.0/. */
+
 /** */
 class SRAGMap {
+  constructor() {
+    this.fluColors = {
+      1: 'green',
+      2: 'yellow',
+      3: 'orange',
+      4: 'red',
+      };
+    // create the tile layer with correct attribution
+    this.map = L.map('map');
+    this.osmUrl='http://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png';
+    this.osmAttrib='Map data © <a href="http://openstreetmap.org">' +
+      'OpenStreetMap</a> contributors';
+    this.osm = new L.TileLayer(
+      this.osmUrl, {minZoom: 1, maxZoom: 12, attribution: this.osmAttrib}
+    );
 
-    /**
-     * Build a map
-     *
-     * @param {dict} geoJsonBr - geoJson data about Brazilian territory
-     * @param {dict} srag_data - srag data
-     */
-    static makeMap(
-        map, geoJsonBr, srag_data, year, week
-    ) {
+    this.map.setView([-16, -50.528742], 3)  ;
+    this.osm.addTo(this.map);
+  }
 
-        // remove existent layer from the map
-        map.eachLayer(function (layer) {
-            if ('feature' in layer) {
-                map.removeLayer(layer);
-            }
-        });
+  /**
+   * Build a map
+   *
+   * @param {dict} geoJsonBr - geoJson data about Brazilian territory
+   * @param {dict} sragData - srag data
+   */
+  makeMap(
+    geoJsonBr, sragData, year, week, clickExternalTrigger
+  ) {
+    var fluColors = this.fluColors;
+    var map = this.map;
+    // remove existent layer from the map
+    this.map.eachLayer(function (layer) {
+      if ('feature' in layer) {
+        map.removeLayer(layer);
+      }
+    });
 
-        // apply onclick event on each region/state on the map
-        function onEachFeature(feature, layer) {
-            //bind click
-            layer.on({
-                click: function(){
-                    // reset line weight
-                    geojson_layer.eachLayer(function (_layer) {
-                        _layer.setStyle({
-                            weight: 1
-                        });
-                    });
-
-                    var state_name = feature.properties.nome;
-                    var week = parseInt($('#week').val() || 0);
-                    var year = parseInt($('#year').val() || 0);
-
-                    if ($('#selected_state').val() == state_name) {
-                        state_name = '';
-                        $('.incidence-chart-title').text('');
-                    } else {
-                        // bold the selected state
-                        layer.setStyle({
-                            weight: 2
-                        });
-                        $('.incidence-chart-title').text(' - ' + state_name);
-                    }
-
-                    $('#selected_state').val(state_name);
-
-                    plot_incidence_chart(year, state_name, week);
-                    makeTable(year, week, state_name);
-                }
+    // apply onclick event on each region/state on the map
+    function onEachFeature(feature, layer) {
+      //bind click
+      layer.on({
+        click: function(){
+          // reset line weight
+          geojsonLayer.eachLayer(function (_layer) {
+            _layer.setStyle({
+              weight: 1
             });
-        };
+          });
 
-        var selected_state = $('#selected_state').val();
+          var stateName = feature.properties.nome;
+          var week = parseInt($('#week').val() || 0);
+          var year = parseInt($('#year').val() || 0);
 
-        // show geojson on the map
-        geojson_layer = L.geoJson(br_states, {
-            onEachFeature: onEachFeature,
-            style: function(feature) {
-                l_name = feature.properties.nome;
-
-                style_properties= {
-                    fillColor: '#ffffff',
-                    color: '#333333',
-                    fillOpacity: 0.5,
-                    weight: 1,
-                };
-
-                week_state = $.grep(srag_data, function(n,i){
-                    return (
-                        n.unidade_da_federacao===l_name &&
-                        n.isoweek===week
-                    );
-                })[0];
-
-                style_properties['fillColor'] = flu_colors[week_state['alert']];
-
-                if (selected_state==l_name) {
-                    style_properties['weight'] = 2;
-                }
-                return style_properties;
-            }
-        });
-        geojson_layer.addTo(map);
-    }
-
-
-    /**
-     * Change the color of the map using the alerts criteria
-     *
-     * @param {dict} df - data frame object
-     */
-    function changeColorMap(df) {
-        var state = $('#selected_state').val();
-        var week = parseInt($('#week').val() || 0);
-        var style_properties= {
-            fillColor: '#ffffff',
-            color: '#333333',
-            fillOpacity: 0.5,
-            weight: 1,
-        };
-
-        if(week>0) {
-            // when by week criteria is selected
-            df_alert = $.grep(df, function(n,i){
-                return n.isoweek===week
+          if ($('#selected-state').val() == stateName) {
+            stateName = '';
+            $('.incidence-chart-title').text('');
+          } else {
+            // bold the selected state
+            layer.setStyle({
+              weight: 2
             });
-            geojson_layer.eachLayer(function (layer) {
-                l_name = layer.feature.properties.nome;
+            $('.incidence-chart-title').text(' - ' + stateName);
+          }
 
-                layer.setStyle(style_properties);
-                if (l_name==state) {
-                    layer.setStyle({'weight': 2});
-                }
+          $('#selected-state').val(stateName);
 
-                df_alert_state = $.grep(df_alert, function(n,i){
-                    return n.unidade_da_federacao===l_name
-                })[0];
-
-                layer.setStyle({
-                    fillColor: flu_colors[df_alert_state['alert']]
-                });
-            });
-        } else {
-            // when whole year criteria is selected
-            geojson_layer.eachLayer(function (layer) {
-                l_name = layer.feature.properties.nome;
-
-                layer.setStyle(style_properties);
-                if (l_name==state) {
-                    layer.setStyle({'weight': 2});
-                }
-
-                alerts = {
-                    1: 0,
-                    2: 0,
-                    3: 0,
-                    4: 0
-                };
-
-                df_alert_state = $.grep(df, function(n,i){
-                    return n.unidade_da_federacao===l_name
-                });
-
-
-                $(df_alert_state).each(function(i){
-                    ++alerts[df_alert_state[i]['alert']];
-                });
-
-                layer.setStyle({
-                    fillColor: flu_colors[getAlertLevelForWholeYear(alerts)]
-                });
-
-            });
+          clickExternalTrigger(year, stateName, week);
         }
+      });
+    };
+
+    var selectedState = $('#selected-state').val();
+
+    // show geojson on the map
+    var geojsonLayer = L.geoJson(geoJsonBr, {
+      onEachFeature: onEachFeature,
+      style: function(feature) {
+        var layerName = feature.properties.nome;
+
+        var styleProperties= {
+          fillColor: '#ffffff',
+          color: '#333333',
+          fillOpacity: 0.5,
+          weight: 1,
+        };
+
+        var weekState = $.grep(sragData, function(n,i){
+          return (
+            n.unidade_da_federacao===layerName &&
+            n.isoweek===week
+          );
+        })[0];
+
+        styleProperties['fillColor'] = fluColors[weekState['alert']];
+
+        if (selectedState==layerName) {
+          styleProperties['weight'] = 2;
+        }
+        return styleProperties;
+      }
+    });
+    geojsonLayer.addTo(map);
+  }
+
+
+  /**
+   * Change the color of the map using the alerts criteria
+   *
+   * @param {dict} df - data frame object
+   */
+  changeColorMap(df) {
+    var state = $('#selected-state').val();
+    var week = parseInt($('#week').val() || 0);
+    var styleProperties= {
+      fillColor: '#ffffff',
+      color: '#333333',
+      fillOpacity: 0.5,
+      weight: 1,
+    };
+
+    if(week>0) {
+      // when by week criteria is selected
+      df_alert = $.grep(df, function(n,i){
+        return n.isoweek===week
+      });
+      geojsonLayer.eachLayer(function (layer) {
+        layerName = layer.feature.properties.nome;
+
+        layer.setStyle(styleProperties);
+        if (layerName==state) {
+          layer.setStyle({'weight': 2});
+        }
+
+        df_alert_state = $.grep(df_alert, function(n,i){
+          return n.unidade_da_federacao===layerName
+        })[0];
+
+        layer.setStyle({
+          fillColor: fluColors[df_alert_state['alert']]
+        });
+      });
+    } else {
+      // when whole year criteria is selected
+      geojsonLayer.eachLayer(function (layer) {
+        layerName = layer.feature.properties.nome;
+
+        layer.setStyle(styleProperties);
+        if (layerName==state) {
+          layer.setStyle({'weight': 2});
+        }
+
+        alerts = {
+          1: 0,
+          2: 0,
+          3: 0,
+          4: 0
+        };
+
+        df_alert_state = $.grep(df, function(n,i){
+          return n.unidade_da_federacao===layerName
+        });
+
+
+        $(df_alert_state).each(function(i){
+          ++alerts[df_alert_state[i]['alert']];
+        });
+
+        layer.setStyle({
+          fillColor: fluColors[getAlertLevelForWholeYear(alerts)]
+        });
+      });
     }
+  }
 }
