55977a6ad6000b98df7c93193745fea3a21de3e8
Ivan Ogasawara <ivan.ogasawara@gmail.com>
2017-10-24 17:42:06 -0400

    Fixes #101.

--- fludashboard/runwsgi.sh
+++ fludashboard/runwsgi.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-DEPLOY_HOME="/tmp/"
+FLU_HOME=$(python -c "import fludashboard, os; print(os.path.dirname(fludashboard.__file__))")
 NUM_WORKERS=4
 
 WSGI_HOST="0.0.0.0"
@@ -7,12 +7,14 @@ WSGI_PORT="8000"
 WSGI_BIND="$WSGI_HOST:$WSGI_PORT"
 WSGI_APP="fludashboard.app:app"
 
-ERRLOG="$DEPLOY_HOME/fludashboard_error.log"
-ACCESS_LOG="$DEPLOY_HOME/logs/wsgi_server.access"
+echo '[II] Creating log folder ...'
+mkdir $FLU_HOME/log
+
+FLU_LOG="$FLU_HOME/log/fludashboard.log"
 
 echo '[II] Starting update data ...'
 python -c "from fludashboard.app import update_data_before_startup as u; u()"
 echo '[II] Starting update data [OK]'
 
 echo '[II] Starting app ...'
-gunicorn -w $NUM_WORKERS -b $WSGI_BIND --log-file=$ERRLOG $WSGI_APP
+gunicorn -w $NUM_WORKERS -b $WSGI_BIND --log-file=$FLU_LOG $WSGI_APP
