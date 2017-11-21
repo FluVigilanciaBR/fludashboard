54de5cfd37183ca0f71406311f52fc09ac2c0ed4
Ivan Ogasawara <ivan.ogasawara@gmail.com>
2017-11-20 21:45:47 -0400

    Improving settings file (90%)

--- fludashboard/runwsgi.py
+++ fludashboard/runwsgi.py
@@ -1,6 +1,10 @@
-import os
+from . import settings
 
-if __name__ == '__main__':
+import os
+import sys
+
+
+def startup():
     try:
         import gunicorn
     except:
@@ -8,5 +12,22 @@ if __name__ == '__main__':
 
     path_root = os.path.dirname(os.path.abspath(__file__))
     path_file = os.path.join(path_root, 'runwsgi.sh')
-    os.system('bash %s' % path_file)
 
+    for setting_variable in dir(settings):
+        if setting_variable.startswith("__"):
+            continue
+
+        v = settings.__dict__[setting_variable]
+
+        # apply just for string variables
+        if isinstance(v, str):
+            os.environ[setting_variable] = v
+
+    os.environ['PYTHONPATH'] = os.path.dirname(sys.executable)
+    os.environ['PATH'] += ':' + os.environ['PYTHONPATH']
+    os.system('python --version')
+    os.system('/bin/bash %s' % path_file)
+
+
+if __name__ == '__main__':
+    startup()
