653d3014406b14bc8b9a56824ed1616f8112d763
Ivan Ogasawara <ivan.ogasawara@gmail.com>
2018-01-30 22:44:46 -0400

    Resolves partially #115

--- fludashboard/libs/views.py
+++ fludashboard/libs/views.py
@@ -41,6 +41,21 @@ def compose_data_url(variables: str):
     return '/'.join(url)
 
 
+@app.errorhandler(404)
+def page_not_found(e):
+    return render_template('404.html'), 404
+
+
+@app.errorhandler(403)
+def page_forbidden(e):
+    return render_template('403.html'), 403
+
+
+@app.errorhandler(500)
+def page_error_server(e):
+    return render_template('500.html'), 500
+
+
 @app.route("/super-header")
 def super_header():
     return render_template("super-header.html")
