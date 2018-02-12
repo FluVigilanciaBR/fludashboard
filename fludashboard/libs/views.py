2eae34705231764846ca0803408c00f800c76429
Marcelo F C Gomes <marfcg@gmail.com>
2018-02-12 12:09:50 -0200

    High priority bug fix: - Year list shows all years available in database. #120
    Persistent high priority bug:
    - Years < 2017 are not displayed. Traceback reported in #120

--- fludashboard/libs/views.py
+++ fludashboard/libs/views.py
@@ -73,7 +73,7 @@ def index():
 
     # read data to get the list of available years
     df = fluDB.read_data(
-        table_name='historical_estimated_values',
+        table_name='current_estimated_values',
         dataset_id=1, scale_id=1, territory_id=0
     )
 
@@ -81,8 +81,8 @@ def index():
     # By default should be the current or latest available
     list_of_years = list(set(df.epiyear))
 
-    epiyear = df.base_epiyear.max()
-    epiweek = df.base_epiweek.max()
+    epiyear = df.epiyear.max()
+    epiweek = df.epiweek[df.epiyear == epiyear].max()
 
     last_week_years = {
         y: calc_last_epiweek(y) for y in list_of_years
